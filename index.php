2883f061f88623d8446a6216b22a6e1ca13ecdef
castawaypirate <apsikiotis@gmail.com>
2023-10-31 16:40:30 +0200

    Improve validateRequest function

--- index.php
+++ index.php
@@ -27,20 +27,22 @@ $dbConnection = $database->getConnection();
 
 $routes = [];
 route('/', function () {
-    echo 'Hello World';
+    echo 'Hello World!';
 });
 
 route('/api/users/access', function () use ($dbConnection){
-    $request = validateRequest('POST');
-    $userController = new UserController($dbConnection);
-    $result = $userController->access($request);
-    $jsonResult = json_encode($result);
-    header('Content-Type: application/json; charset=utf-8');
-    echo $jsonResult;
+    $request = validateRequest('POST', 'JSON', '');
+    if ($request) {
+        $userController = new UserController($dbConnection);
+        $result = $userController->access($request);
+        $jsonResult = json_encode($result);
+        header('Content-Type: application/json; charset=utf-8');
+        echo $jsonResult;
+    }
 });
 
 route('/api/users/validateUser', function () use ($dbConnection){
-    $validated = validateRequest('GET');
+    $validated = validateRequest('GET', '', 'Bearer');
     if($validated) {
         $userController = new UserController($dbConnection);
         $result = $userController->validateUser();
@@ -51,19 +53,8 @@ route('/api/users/validateUser', function () use ($dbConnection){
 });
 
 route('/api/posts/showAll', function () use ($dbConnection) {
-    if (isset($_SERVER['CONTENT_TYPE']) && strpos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $validated = true;           
-        } else {
-            http_response_code(405); // Method Not Allowed
-            return false; 
-        }
-    }
-    else {
-        http_response_code(415); // Unsupported Media Type
-        return false;
-    }
-    if ($validated) {
+    $request = validateRequest('GET', 'JSON', '');
+    if ($request) {
         $postController = new PostController($dbConnection);
         $result = $postController->showAllPosts();
         $jsonResult = json_encode($result);
@@ -73,9 +64,8 @@ route('/api/posts/showAll', function () use ($dbConnection) {
 });
 
 route('/api/posts/create', function () use ($dbConnection) {
-    $request = validateRequest('POST');
+    $request = validateRequest('POST', 'JSON', 'Bearer');
     if($request){
-        error_log('resttset');
         $userController = new UserController($dbConnection);
         $result = $userController->validateUser();
         $success = $result['success'];
@@ -121,45 +111,48 @@ function run() {
     }
 }
 
-function validateRequest(string $requestMethod){
-    if ($_SERVER['REQUEST_METHOD'] === $requestMethod) {
-        // Check the Content-Type header for JSON
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SERVER['CONTENT_TYPE']) && strpos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {
-            // Get the raw JSON data from the request body
-            $jsonData = file_get_contents('php://input');
-            // Attempt to decode the JSON data into a PHP associative array
-            $data = json_decode($jsonData, true);
-            // Check if the JSON data was successfully decoded
-            if ($data === null) {
-                // JSON parsing failed, handle the error and return a response
-                http_response_code(400); // Bad Request
-                // echo json_encode(array('error' => 'Invalid JSON data')).PHP_EOL;
-                exit();
-            }
-            // Respond with a success message
-            http_response_code(200); // OK
-            // echo json_encode(array('message' => 'Data received successfully')).PHP_EOL;
-            return $data;
-        } else if ($_SERVER['REQUEST_METHOD'] === 'GET' or $_SERVER['REQUEST_METHOD'] === 'PUT' or $_SERVER['REQUEST_METHOD'] === 'DELETE') {
-            if (!empty($_SERVER['HTTP_AUTHORIZATION'])) {
-                // Handle GET requests without JSON data (You can add further validation if needed)
-                http_response_code(200); // OK
-                // echo json_encode(array('message' => 'GET request successful')).PHP_EOL;
-                return true;
-            } else {
-                http_response_code(401);
-                return false;
-            }            
+function validateRequest($requestMethod, $contentType = '', $authorization = '') {
+    if ($_SERVER['REQUEST_METHOD'] !== $requestMethod) {
+        // Method Not Allowed
+        http_response_code(405);
+        return false;
+    }
+
+    if ($contentType === 'JSON') {
+        if (!isset($_SERVER['CONTENT_TYPE'])){
+            // Bad Request
+            http_response_code(400);
+            return false;
         } else {
-            http_response_code(415); // Unsupported Media Type
-            // echo json_encode(array('error' => 'Unsupported Media Type: Expecting application/json')).PHP_EOL;
+            if (strpos($_SERVER['CONTENT_TYPE'], 'application/json') === false) {
+                // Unsupported Media Type
+                http_response_code(415);
+                return false;
+            }
+        }
+    }
+
+    if ($authorization === 'Bearer') {
+        if (empty($_SERVER['HTTP_AUTHORIZATION'])) {
+            //Unauthorized
+            http_response_code(401);
             return false;
         }
-    } else {
-        // If the request method is not POST, return an error response
-        http_response_code(405); // Method Not Allowed
-        // echo json_encode(array('error' => 'Method not allowed')).PHP_EOL;
-        return false;
+    }
+
+    if ($requestMethod === 'POST') {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        if ($data === null) {
+            // Bad Request (Invalid JSON data)
+            http_response_code(400);
+            return false;
+        }
+        return $data;
+    }
+
+    if ($requestMethod === 'GET') {
+        return true;
     }
 }
 ?>
